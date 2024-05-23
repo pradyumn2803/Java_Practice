c742951664e0568133e3c0e8e430148123a7bde1
Pradyumn Choudhary <pradyumnchoudhary123@gmail.com>
2024-05-23 19:10:38 +0530

    modified merge strings

--- mergeString.java
+++ mergeString.java
@@ -3,9 +3,12 @@ public class mergeString {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         String[] s=sc.nextLine().split(",");
-        String add=sc.nextLine();
-        s=Arrays.copyOf(s,s.length+1);
-        s[s.length-1]=add;
+        String[] add=sc.nextLine().split(",");
+        
+        for(int i=0;i<add.length;i++){
+            s=Arrays.copyOf(s,s.length+1);
+            s[s.length-1]=add[i];
+        }
 
         for(int i=0;i<s.length;i++){
             System.out.print(s[i]);
